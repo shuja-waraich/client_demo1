d845d41e1fbc4633ad10890b0882debd7fb02fdd
shuja <53631362+shuja-waraich@users.noreply.github.com>
2023-05-16 16:10:04 +0500

    Update utils.py

--- knowledge_gpt/utils.py
+++ knowledge_gpt/utils.py
@@ -1,6 +1,6 @@
 import re
 from io import BytesIO
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Union
 
 import docx2txt
 import streamlit as st
@@ -52,7 +52,7 @@ def parse_txt(file: BytesIO) -> str:
 
 
 @st.cache(allow_output_mutation=True)
-def text_to_docs(text: str | List[str]) -> List[Document]:
+def text_to_docs(text: Union[str, List[str]]) -> List[Document]:
     """Converts a string or list of strings to a list of Documents
     with metadata."""
     if isinstance(text, str):
